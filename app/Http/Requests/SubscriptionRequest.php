bd806f45a1a916263d6010a6b2e20f061da56da5
natec23 <natec23@gmail.com>
2026-09-20 11:40:17 -0500

    🎉 initial commit with laravel 13.x

---
+++ app/Http/Requests/SubscriptionRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Subscriptions;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SubscriptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'service_id' => 'required|integer|exists:services,id',
+            'start_date' => 'date',
+            'end_date' => 'nullable|date',
+            'price' => 'nullable|decimal:0,2',
+            'billing_cycle' => 'integer',
+        ];
+    }
+}
