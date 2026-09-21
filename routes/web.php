3aea038c0eb454704c41390fcf11998a251803b2
natec23 <natec23@gmail.com>
2026-09-21 18:03:19 -0500

    remove welcome

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,8 @@ use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
-
 Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', DashboardController::class)->name('home');
     Route::get('dashboard', DashboardController::class)->name('dashboard');
 
     Route::resource('services', ServiceController::class);
